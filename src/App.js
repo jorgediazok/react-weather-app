fea9fe94b7180867b292606721846a30d8badff2
Jorge <jorgediazok@gmail.com>
2020-02-23 21:23:34 -0300

    formulario validado

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import WeatherForm from './components/WeatherForm';
 import { WEATHER_KEY } from './keys';
 
 class App extends Component {
+  state = {
+    temperature: '',
+    description: '',
+    humidity: '',
+    wind_speed: '',
+    city: '',
+    country: '',
+    error: null
+  };
+
   getWeather = async e => {
     e.preventDefault();
     const { city, country } = e.target.elements;
     const cityValue = city.value;
     const countryValue = country.value;
-    const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`;
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data);
+
+    if (cityValue && countryValue) {
+      const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data);
+
+      this.setState({
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        humidity: data.main.humidity,
+        wind_speed: data.wind.speed,
+        city: data.name,
+        country: data.sys.country,
+        error: null
+      });
+    } else {
+      this.setState({ error: 'Please Enter a City and a Country' });
+    }
   };
   render() {
     return (
@@ -20,7 +45,7 @@ class App extends Component {
         <div className="row">
           <div className="col-md-4 mx-auto">
             <WeatherForm getWeather={this.getWeather} />
-            <WeatherInfo />
+            <WeatherInfo {...this.state} />
           </div>
         </div>
       </div>
